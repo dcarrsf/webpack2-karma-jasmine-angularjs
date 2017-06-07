1023fe92dbe3e9ccd63b004a84ffc22d8e215d68
dcarrsf <dan@dancarr.co>
2017-06-07 14:23:55 -0700

    Adding routes and route configuration.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import angular from 'angular';
-import './services/utils/calc';
+import ngRoute from 'angular-route';
+import './services/utils/';
+import './controllers/pages/';
+import './directives/pages/';
 import './app.scss';
 
-// Link to app
-angular.module('ng-test', ['api.utils'])
+// Link to app and dependencies
+angular.module('loginApp', [
+  'api.utils', 
+  'api.controllers',
+  'api.directives',
+  'ngRoute'
+])
 // Set up main controller
-.controller('ng-test-controller', ['$scope', 'Calc', ($scope, Calc) => {
+.controller('mainCtrl', ['$scope', 'Calc', ($scope, Calc) => {
   $scope.result = `${Calc.add(4, 1)}`;
-}]);
+}])
+// Set up routing
+.config(($routeProvider) => {
+    $routeProvider
+    .when('/', {
+        templateUrl : 'home.html',
+        controller : "homeCtrl"
+    })
+    .when('/login', {
+        templateUrl : 'login.html',
+        controller : "loginCtrl"
+    })
+    .otherwise('/')
+});;
